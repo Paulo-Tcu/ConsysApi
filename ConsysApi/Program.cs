0bfb28d24bfa65357cdeca71b789877d976fb2a9
Paulo Cesar Querino Da Cruz De Araujo <paulo.cruz@linx.com.br>
2024-06-28 09:21:27 -0300

    visual Swagger

--- ConsysApi/Program.cs
+++ ConsysApi/Program.cs
@@ -14,12 +14,15 @@ builder.ConfigurationAuthentication();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
